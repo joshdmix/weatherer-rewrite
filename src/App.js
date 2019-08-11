cc16acc9f9f6d0fa27e9427422fd7cc1a2b65a0f
Josh Mix <joshuadmix@gmail.com>
2019-08-11 11:27:32 -0400

    in process of setting up variable osc, modfreq

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Tone from 'tone';
 
-const synth = new Tone.Synth().toMaster();
-const invokeTone = tone => {
-  synth.triggerAttackRelease(tone, '8n');
-};
-const notes = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
-const octaves = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
-let tones = notes.map(note => octaves.map(octave => `${note}${octave}`));
-const tonesTotal = [];
-octaves.forEach(octave =>
-  tones.forEach(noteArr => tonesTotal.push(noteArr[octave])),
-);
-
-const octaveSpliceArr = [0, 12, 24, 36, 48, 60, 72, 90];
-console.log(tonesTotal);
-const octaveSlice = startIndex => {
-  return tonesTotal.slice(startIndex, startIndex + 12);
-};
-
-const styleNat = {backgroundColor: 'white', height: '120px'};
-const styleFlat = {backgroundColor: 'black', height: '100px', zIndex: 99};
-
 const App = () => {
+  const notes = [
+    'C',
+    'Db',
+    'D',
+    'Eb',
+    'E',
+    'F',
+    'Gb',
+    'G',
+    'Ab',
+    'A',
+    'Bb',
+    'B',
+  ];
+  const octaves = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+  const keycodes = [65, 87, 83, 69, 68, 70, 84, 71, 89, 72, 85, 74, 73];
+  let noteLength = '8n';
+  let tones = notes.map(note => octaves.map(octave => `${note}${octave}`));
+  const tonesTotal = [];
+  octaves.forEach(octave =>
+    tones.forEach(noteArr => tonesTotal.push(noteArr[octave])),
+  );
+
+  const octaveSpliceArr = [0, 12, 24, 36, 48, 60, 72, 90];
+  console.log(tonesTotal);
+  const octaveSlice = startIndex => {
+    return tonesTotal.slice(startIndex, startIndex + 12);
+  };
+
+  const styleNat = {backgroundColor: 'white', height: '120px'};
+  const styleFlat = {backgroundColor: 'black', height: '100px', zIndex: 99};
+
+  const changeNoteLength = note => {};
+  const [oscType, setOscType] = useState('triangle');
+  const [modFreq, setModFreq] = useState('0.3');
+  // const changeOsc = oscType => {
+  //   oscType === 'pwm' ? setOscType('square') : setOscType('pwm');
+  // };
+  const synth = new Tone.Synth({
+    oscillator: {type: oscType, modulationFrequency: modFreq},
+  }).toMaster();
+  const invokeTone = (tone, noteLength) => {
+    synth.triggerAttackRelease(tone, noteLength);
+  };
+
   return (
     <>
       {octaveSpliceArr.map(startValue => {
@@ -32,7 +56,7 @@ const App = () => {
               return [
                 <li style={{display: 'inline', width: '3px', height: '15px'}}>
                   <button
-                    onClick={() => invokeTone(tone)}
+                    onClick={() => invokeTone(tone, noteLength)}
                     style={tone.includes('b') ? styleFlat : styleNat}>
                     {tone}
                   </button>
