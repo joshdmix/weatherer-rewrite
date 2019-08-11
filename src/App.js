00919974db075ec06fddc45d40b7d892c0bc884d
jdm1986 <josh.mix@eltoro.com>
2019-08-11 09:56:54 -0400

    eh

--- src/App.js
+++ src/App.js
@@ -6,35 +6,34 @@ const invokeTone = (tone) => { synth.triggerAttackRelease(tone, "8n") }
 const notes = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
 const octaves = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
 let tones = notes.map(note => octaves.map(octave => `${note}${octave}`))
+const tonesTotal = []
+octaves.forEach(octave => tones.forEach(noteArr => (tonesTotal.push(noteArr[octave]))))
 const octaveSpliceArr = [0, 12, 24, 36, 48, 60, 72, 90]
 const octaveSplice = (startIndex) => tonesTotal.splice(startIndex, startIndex + 12)
 
-const tonesTotal = []
-octaves.forEach(octave => tones.forEach(noteArr => (tonesTotal.push(noteArr[octave]))))
+
+
+
 
 
 
 const App = () => {
 	return (
 					<>
-									{octaveSpliceArr.map(startValue => {
-													octaveSplice(startValue).map(tones => {
-																	<ul style={{ listStyleType: "none", margin: 0, padding: 0 }}>
-																	tones.map(tone => {
-													<li style={{ float: "left"}}>
-																				 <button
-																								 style={{backgroundColor: tone.includes("b") ? "black": "white"}}
-																								 onClick={() => invokeTone(tone)}>{tone}
-																				 </button>
-																 </li>
-																					}
-																					)
-
-												 </ul>
+									{
+													octaveSpliceArr.map(startValue => {
+				<ul>
+								{
+												octaveSplice(startValue).map(tone =>
+																{
+																				<li>
+																								<button>{tone}</button>
+																				</li>
+																}
+								)}
+				</ul>
+									})
 													}
-													)
-									}
-								  )}
 
 	</>
 	)
