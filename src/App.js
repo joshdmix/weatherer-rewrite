954e19df7ee8db361f095d998a5b027eebda81ac
jdm1986 <josh.mix@eltoro.com>
2019-08-10 20:14:08 -0400

    gotta reel in this nested mapping

--- src/App.js
+++ src/App.js
@@ -6,23 +6,36 @@ const invokeTone = (tone) => { synth.triggerAttackRelease(tone, "8n") }
 const notes = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
 const octaves = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
 let tones = notes.map(note => octaves.map(octave => `${note}${octave}`))
+const octaveSpliceArr = [0, 12, 24, 36, 48, 60, 72, 90]
+const octaveSplice = (startIndex) => tonesTotal.splice(startIndex, startIndex + 12)
+
 const tonesTotal = []
 octaves.forEach(octave => tones.forEach(noteArr => (tonesTotal.push(noteArr[octave]))))
-let color = "white"
+
+
 
 const App = () => {
 	return (
-	<>
-    <ul style={{ listStyleType: "none", margin: 0, padding: 0 }}>
-									{tonesTotal.map(tone =>
+					<>
+									{octaveSpliceArr.map(startValue => {
+													octaveSplice(startValue).map(tones => {
+																	<ul style={{ listStyleType: "none", margin: 0, padding: 0 }}>
+																	tones.map(tone => {
 													<li style={{ float: "left"}}>
 																				 <button
 																								 style={{backgroundColor: tone.includes("b") ? "black": "white"}}
 																								 onClick={() => invokeTone(tone)}>{tone}
 																				 </button>
 																 </li>
+																					}
+																					)
+
+												 </ul>
+													}
+													)
+									}
 								  )}
-     </ul>
+
 	</>
 	)
 }
