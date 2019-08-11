eefe9e6ca9e93c6bdf777c17ed65f72e368f4d8e
Josh Mix <joshuadmix@gmail.com>
2019-08-11 10:57:55 -0400

    7th octave + acting weird, need to adjust keys so flats on top of nats

--- src/App.js
+++ src/App.js
@@ -2,41 +2,48 @@ import React from 'react';
 import Tone from 'tone';
 
 const synth = new Tone.Synth().toMaster();
-const invokeTone = (tone) => { synth.triggerAttackRelease(tone, "8n") }
-const notes = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
-const octaves = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
-let tones = notes.map(note => octaves.map(octave => `${note}${octave}`))
-const tonesTotal = []
-octaves.forEach(octave => tones.forEach(noteArr => (tonesTotal.push(noteArr[octave]))))
-const octaveSpliceArr = [0, 12, 24, 36, 48, 60, 72, 90]
-const octaveSplice = (startIndex) => tonesTotal.splice(startIndex, startIndex + 12)
+const invokeTone = tone => {
+  synth.triggerAttackRelease(tone, '8n');
+};
+const notes = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
+const octaves = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+let tones = notes.map(note => octaves.map(octave => `${note}${octave}`));
+const tonesTotal = [];
+octaves.forEach(octave =>
+  tones.forEach(noteArr => tonesTotal.push(noteArr[octave])),
+);
 
+const octaveSpliceArr = [0, 12, 24, 36, 48, 60, 72, 90];
+console.log(tonesTotal);
+const octaveSlice = startIndex => {
+  return tonesTotal.slice(startIndex, startIndex + 12);
+};
 
-
-
-
-
+const styleNat = {backgroundColor: 'white', height: '120px'};
+const styleFlat = {backgroundColor: 'black', height: '100px', zIndex: 99};
 
 const App = () => {
-	return (
-					<>
-									{
-													octaveSpliceArr.map(startValue => {
-				<ul>
-								{
-												octaveSplice(startValue).map(tone =>
-																{
-																				<li>
-																								<button>{tone}</button>
-																				</li>
-																}
-								)}
-				</ul>
-									})
-													}
-
-	</>
-	)
-}
+  return (
+    <>
+      {octaveSpliceArr.map(startValue => {
+        return [
+          <ul style={{listStyle: 'none', height: '120px'}}>
+            {octaveSlice(startValue).map(tone => {
+              return [
+                <li style={{display: 'inline', width: '3px', height: '15px'}}>
+                  <button
+                    onClick={() => invokeTone(tone)}
+                    style={tone.includes('b') ? styleFlat : styleNat}>
+                    {tone}
+                  </button>
+                </li>,
+              ];
+            })}
+          </ul>,
+        ];
+      })}
+    </>
+  );
+};
 
 export default App;
